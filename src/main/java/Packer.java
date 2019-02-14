a0748a0b786081c60343a85d4b6c459111c43da8
jb-kerboeuf <jb.kerboeuf@gmail.com>
2019-02-14 16:04:46 +0100

    Added Acceptance test and unit test

--- src/main/java/Packer.java
+++ src/main/java/Packer.java
@@ -1,2 +1,5 @@
 public class Packer {
+    public String pack(String articlesChain) {
+        return articlesChain;
+    }
 }
--- src/test/java/PackerTest.java
+++ src/test/java/PackerTest.java
@@ -1,2 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PackerTest {
+    @Test
+    public void acceptanceNaivePacker() {
+        Packer packer = new Packer();
+        Assert.assertEquals("163/8/41/6/8/9/52/5/7/73", packer.pack("163841689525773"));
+    }
+
+    @Test
+    public void shouldMakeOnePackage() {
+        Packer packer = new Packer();
+        Assert.assertEquals("16", packer.pack("16"));
+    }
 }
