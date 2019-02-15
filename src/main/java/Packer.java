ab7a73b1ea84bc859a2b36eb1a96113310d6fb8c
jb-kerboeuf <jb.kerboeuf@gmail.com>
2019-02-15 13:35:14 +0100

    Refacto: extracting magic number and renaming

--- src/main/java/Packer.java
+++ src/main/java/Packer.java
@@ -2,21 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Packer {
-    public String pack(String articlesChain) {
+
+    private static final int MAX_PACK_WEIGHT = 10;
+
+    public String standardPacking(String articlesChain) {
+        List<Integer> articlesWeights = getArticlesWeightsFromChain(articlesChain);
+
+        String packedChain = "";
+        int packWeight = 0;
+        for (int articleWeight : articlesWeights) {
+            if (packWeight + articleWeight > MAX_PACK_WEIGHT) {
+                packedChain += "/";
+                packWeight = 0;
+            }
+            packWeight += articleWeight;
+            packedChain += articleWeight;
+        }
+        return packedChain;
+    }
+
+    private List<Integer> getArticlesWeightsFromChain(String articlesChain) {
         List<Integer> articlesWeights = new ArrayList<>();
         for (String article : articlesChain.split("")) {
             articlesWeights.add(Integer.parseInt(article));
         }
-        String packedChain = "";
-        int sum = 0;
-        for (int articleWeight : articlesWeights) {
-            sum += articleWeight;
-            if (sum > 10) {
-                packedChain += "/";
-                sum = articleWeight;
-            }
-            packedChain += articleWeight;
-        }
-        return packedChain;
+        return articlesWeights;
     }
 }
--- src/test/java/PackerTest.java
+++ src/test/java/PackerTest.java
@@ -5,18 +5,18 @@ public class PackerTest {
     @Test
     public void acceptanceNaivePacker() {
         Packer packer = new Packer();
-        Assert.assertEquals("163/8/41/6/8/9/52/5/7/73", packer.pack("163841689525773"));
+        Assert.assertEquals("163/8/41/6/8/9/52/5/7/73", packer.standardPacking("163841689525773"));
     }
 
     @Test
     public void shouldMakeOnePackage() {
         Packer packer = new Packer();
-        Assert.assertEquals("16", packer.pack("16"));
+        Assert.assertEquals("16", packer.standardPacking("16"));
     }
 
     @Test
     public void shouldMakeTwoPackages() {
         Packer packer = new Packer();
-        Assert.assertEquals("9/6", packer.pack("96"));
+        Assert.assertEquals("9/6", packer.standardPacking("96"));
     }
 }
